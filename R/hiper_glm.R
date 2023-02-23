3d87c619d5de4ff10df8cdd29a633093721a7dea
JianingYao <jianingyao619@gmail.com>
2023-02-22 23:09:10 -0500

    include MLE for linear model

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -1,14 +1,23 @@
 #' @export
-hiper_glm <- function(design, outcome, model = 'linear') {
+hiper_glm <- function(design, outcome, model = 'linear', method = "BFGS") {
   supported_model <- c('linear', 'logit')
   if (! (model %in% supported_model)){
     stop(sprintf("The model %s is not supported.", model))
   }
-  warning("The function is yet to be implemented.")
-  # TODO: model fitting by MLE
-  hglm_out <- list()
-  class(hglm_out) <- 'hglm'
-  return(hglm_out)
+  if (model == 'linear') {
+    hglm_out <- list()
+    class(hglm_out) <- 'hglm'
+    return(hglm_out)
+    if (method == "pinv") {
+      hglm_out$coef <- lm_mle_pinv(design, outcome)
+    }
+    if (method == "BFGS") {
+      hglm_out$coef <- lm_mle_BFGS(design, outcome)
+    }
+  }
+  else {
+    warning("The function is yet to be implemented.")
+  }
 }
 
 
